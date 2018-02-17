d47bd9e62da857709734747d9cda1352396d5d31
Pierre-Yves Aillet <pyaillet@gmail.com>
2018-02-17 02:43:45 +0100

    Refining usage of packet.net server

--- app/create.sh
+++ app/create.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-API_KEY=$(cat api.key)
+export API_KEY=$(cat api.key)
 
 terraform init
 
 terraform apply -auto-approve -var "auth_token=${API_KEY}" -var "project_id=${PROJECT_ID}" .
+
+IP_ADDRESS=$(curl -H "X-Auth-Token: ${API_KEY}" https://api.packet.net/projects/${PROJECT_ID}/devices | jq -r '.devices[0].ip_addresses[0].address')
+echo "The VM IP address is: ${IP_ADDRESS}"
+echo "you can connect to it by launching: "
+echo "ssh ${IP_ADDRESS}"
+
